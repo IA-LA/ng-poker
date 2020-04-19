86b4a6636a0bb63dfdad50819721bcfdb330acff
FJ <fsanchez@lsi.uned.es>
2020-04-19 10:01:05 +0200

    Incorpora usuarios a la sala. Incorpora reparto de cartas a la sala.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,13 +7,20 @@ var pokerControllers = angular.module('pokerControllers', []);
 pokerControllers.controller('PokerHomeCtrl', ['$scope',
     function($scope) {
         $scope.cards = [2,3,4,5,6];
-        $scope.game = 'Poker';
+        $scope.game = 'Poker (Texas Holdem)';
+        $scope.name = 'Sala 1';
     }]);
 
-pokerControllers.controller('PokerRoomCtrl', ['$scope', 'pokerConfig',
-    function($scope, pokerConfig) {
+pokerControllers.controller('PokerRoomCtrl', ['$scope', '$http', 'pokerConfig',
+    function($scope, $http, pokerConfig) {
 
-        $scope.reset = function() {
+        $scope.init = function() {
+            if(!$scope.$$phase) {
+                //$digest or $apply
+                $scope.$apply();
+                $scope.$digest();
+            }
+
             $scope.imagePath = pokerConfig.imagePath;
             $scope.imageExt = pokerConfig.imageExt;
 
@@ -23,25 +30,75 @@ pokerControllers.controller('PokerRoomCtrl', ['$scope', 'pokerConfig',
                 $scope.tableCards.push($scope.shuffleCards.pop());
             }
             $scope.tableCards.push(pokerConfig.emptyCard, pokerConfig.emptyCard);
-            console.log($scope.tableCards.length);
+            console.log('CARTAS', $scope.tableCards.length);
 
+            /* TODO v 0.0.2 */
             $scope.pokerTable = pokerConfig.pokerTable;
             $scope.pokerDealer = pokerConfig.pokerDealer;
+
+            $scope.users = []; //['Fulano', 'Mengano', 'Zutano']; // Initialize with an empty array
+            $scope.moneys = []; //[500, 500, 500]; // Initialize with an empty array
+
+            $http.get('partials/jugadores.json').success(function(data, status, headers, config) {
+                // When the request is successful, add value to $scope.users
+                console.log('Users', data.ususarios.length);
+                for (var i = 0; i < data.ususarios.length; i++) {
+                    // $scope.users = data.split(',')
+                    $scope.users.push(data.ususarios[i].name);
+                    $scope.moneys.push(data.ususarios[i].money);
+                }
+            })
+        };
+
+        $scope.reset = function() {
+            $scope.imagePath = pokerConfig.imagePath;
+            $scope.imageExt = pokerConfig.imageExt;
+
+            // Baraja
+            $scope.shuffleCards = Shuffle(pokerConfig.cardsArr).slice();
+            // Reparte
+            console.log('CARTAS', $scope.tableCards)
+            if($scope.tableCards === undefined)
+            {
+                $scope.tableCards = [];
+                for (var i = 0; i < 3; i++) {
+                    $scope.tableCards.push($scope.shuffleCards.pop());
+                }
+                $scope.tableCards.push(pokerConfig.emptyCard, pokerConfig.emptyCard);
+                console.log('N CARTAS', $scope.tableCards.length);
+            }
+
+            /* TODO v 0.0.2 */
+            $scope.pokerTable = pokerConfig.pokerTable;
+            $scope.pokerDealer = pokerConfig.pokerDealer;
+
+            $scope.cards = []; //[['2c' , '2d'], ['2c' , '2d'], ['2c' , '2d']]; // Initialize with an empty array
+
+            for (var i = 0; i < $scope.users.length; i++) {
+                $scope.cards.push([$scope.shuffleCards.pop(), $scope.shuffleCards.pop()]);
+            }
         };
 
         $scope.next = function() {
             for (var i = 0, length = $scope.tableCards.length; i < length; i++) {
-                if ($scope.tableCards[i] == pokerConfig.emptyCard) {
+                if ($scope.tableCards[i] === pokerConfig.emptyCard) {
                     $scope.tableCards[i] = $scope.shuffleCards.pop();
                     break;
                 }
-                if (i == length - 1) {
+                if (i === length - 1) {
                     alert('Gana esta mano: xxxxx. Y se lleva: XX,XX €');
                 }
             }
             //console.log($scope.tableCards);
-        }
+        };
 
-        $scope.reset();
+        /* TODO v 0.0.2 */
+        $scope.reload = function($scope)
+        {
+            location.reload($scope);
+        };
+
+        // AL CARGAR LA PAGINA
+        $scope.init();
 
     }]);
--- app/js/functions.js
+++ app/js/functions.js
@@ -5,9 +5,18 @@
 function Shuffle(o) {
     for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
-};
+}
 
 function Mano(o) {
     for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
-};
+}
+
+function UserCtrl($scope, $http) {
+    $scope.users = [] // Initialize with an empty array
+
+    $http.get('data/users').success(function(data, status, headers, config) {
+        // When the request is successful, add value to $scope.users
+        $scope.users = data.split(',')
+    })
+}
